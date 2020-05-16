e9fc530e488428248d1c86bc7138ad3aa7a85e64
Rafael Kasper <prof.rafaelkasper@gmail.com>
2020-05-16 20:40:49 -0300

    Update menu_ads.php

--- ads/menu_ads.php
+++ ads/menu_ads.php
@@ -20,10 +20,7 @@
                     <a class="dropdown-item text-white bg-info" href="ads_cadastrar_ano_letivo.php"><i class="fas fa-school text-white"></i>     ANO LETIVO</a>
                     <a class="dropdown-item text-white bg-info" href="ads_cadastrar_disciplinas.php"><i class="fas fa-book text-white"></i>     DISCIPLINA</a>
                     <a class="dropdown-item text-white bg-info" href="ads_cadastrar_equipe_diretiva.php"><i class="far fa-id-card text-white"></i>     EQUIPE DIRETIVA</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_cadastrar_func.php"><i class="fas fa-id-card-alt text-white"></i>     FUNCIONÁRIOS</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_cadastrar_horas_func.php"><i class="fas fa-business-time text-white"></i>     HORAS FUNCIONÁRIOS</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_cadastrar_horas.php"><i class="fas fa-user-clock text-white"></i>    HORAS PROFESSORES</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_cadastrar_planilha_individual.php"><i class="fas fa-book-reader text-white"></i>     PLANILHA DE NOTAS INDIVIDUAL</a>
+                          <a class="dropdown-item text-white bg-info" href="ads_cadastrar_planilha_individual.php"><i class="fas fa-book-reader text-white"></i>     PLANILHA DE NOTAS INDIVIDUAL</a>
                     <a class="dropdown-item text-white bg-info" href="ads_cadastrar_planilha_notas.php"><i class="fas fa-table text-white"></i>   PLANILHA DE NOTAS</a>
                     <a class="dropdown-item text-white bg-info" href="ads_cadastrar_professores.php"><i class="fas fa-chalkboard-teacher text-white"></i>     PROFESSORES</a>
                     <a class="dropdown-item text-white bg-info" href="ads_cadastrar_turmas.php"><i class="fas fa-receipt text-white"></i>     TURMA</a>
@@ -37,8 +34,7 @@
                     <a class="dropdown-item text-white bg-info" href="ads_alterar_ano_letivo.php"><i class="fas fa-school text-white"></i>     ANO LETIVO</a>
                     <a class="dropdown-item text-white bg-info" href="ads_alterar_disciplina.php"><i class="fas fa-book text-white"></i>     DISCIPLINA</a>
                     <a class="dropdown-item text-white bg-info" href="ads_alterar_equipe.php"><i class="far fa-id-card text-white"></i>     EQUIPE DIRETIVA</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_alterar_func.php"><i class="fas fa-id-card-alt text-white"></i>     FUNCIONÁRIOS</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_alterar_planilha_turma.php"><i class="fas fa-table text-white"></i>    PLANILHA DE NOTAS TURMA</a>
+                         <a class="dropdown-item text-white bg-info" href="ads_alterar_planilha_turma.php"><i class="fas fa-table text-white"></i>    PLANILHA DE NOTAS TURMA</a>
                     <a class="dropdown-item text-white bg-info" href="ads_alterar_planilha.php"><i class="fas fa-book-reader text-white"></i>    PLANILHA DE NOTAS</a>
                     <a class="dropdown-item text-white bg-info" href="ads_alterar_professores.php"><i class="fas fa-chalkboard-teacher text-white"></i>     PROFESSORES</a>
                     <a class="dropdown-item text-white bg-info" href="ads_alterar_trimestre.php"><i class="far fa-calendar-times text-white"></i>     TRIMESTRE</a>
@@ -49,21 +45,12 @@
                 <a class="nav-link dropdown-toggle text-white bg-info" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><strong><i class="fas fa-search"></i>     CONSULTAR</strong></a>
                 <div class="dropdown-menu bg-info">
                     <a class="dropdown-item text-white bg-info" href="ads_consultar_formativa.php"><i class="fas fa-graduation-cap text-white"></i>     FORMATIVA</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_consultar_horas_func.php"><i class="fas fa-business-time text-white"></i>     HORAS FUNCIONÁRIOS</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_consultar_horas.php"><i class="fas fa-user-clock text-white"></i>     HORAS PROFESSORES</a>
-                    <a class="dropdown-item text-white bg-info" href="ads_calcular_media_anual.php"><i class="fas fa-medal text-white"></i>     MÉDIA ANUAL</a>
+                     <a class="dropdown-item text-white bg-info" href="ads_calcular_media_anual.php"><i class="fas fa-medal text-white"></i>     MÉDIA ANUAL</a>
                     <a class="dropdown-item text-white bg-info" href="ads_consultar_media.php"><i class="far fa-bookmark text-white"></i>     MÉDIA</a>
                     <a class="dropdown-item text-white bg-info" href="ads_consultar_nota.php"><i class="fas fa-award text-white"></i>     NOTA</a>
                 </div>
             </li>
-            <!--<li class="nav-item dropdown">
-                 <a class="nav-link dropdown-toggle text-white bg-info" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">BACKUP</a>
-                 <div class="dropdown-menu">
-                     <a class="dropdown-item" href="ads_gerar_backup.php">FAZER BACKUP</a>
-                     <a class="dropdown-item" href="ads_importar_backup.php">IMPORTAR BACKUP</a>
-                 </div>
-             </li>
-                !-->
+           
             <li class="nav-item text-white bg-danger pl-3 pr-3" style="float: right;">
                 <a class="nav-link text-white"  href="../functions/logout.php"><strong><i class="fas fa-sign-out-alt"></i>     SAIR</strong></a>
             </li>
